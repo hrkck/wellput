e501ca9f78f53beb48babeb604cb50f55714003f
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-07-05 13:01:18 +0200

    component created TopNavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import './App.css';
+import React, { Component } from "react";
+import TopNavBar from "./Components/core/TopNavBar/TopNavBar";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="text-center">
-        <header className="App-header">
-          <img src={logo} className="col-sm-1 spin" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-            Some changes made.
-        </p>
-        <div className="btn btn-danger">Danger!</div>
-        <div className="container col-sm-2">
-            <div className="alert alert-primary">
-                This is a primary alert — check it out!
+    render() {
+        return (
+            <div>
+                <TopNavBar logo={logo} title="Welcome to the reactive clones" />
+
+                <p className="App-intro">
+                    To get started, edit <code>src/App.jsx</code> and save to reload.
+                </p>
+                <p>Some changes made.</p>
+                <div className="btn btn-danger">Danger!</div>
+                <div className="container col-sm-2">
+                    <div className="alert alert-primary">
+                        This is a primary alert — check it out!
+                    </div>
+                </div>
+                <p>Bootstrap added. works fine.</p>
             </div>
-        </div>
-        <p>
-            Bootstrap added. works fine.
-        </p>
-
-        
-
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
---
+++ src/Components/core/TopNavBar/TopNavBar.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class TopNavBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div className="text-center">
+                <header className="App-header">
+                    <img src={this.props.logo} className="col-sm-1 spin" alt="logo" />
+                    <h1 className="App-title">{this.props.title}</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+TopNavBar.propTypes = {
+    logo: PropTypes.object,
+    title: PropTypes.string
+};
+
+export default TopNavBar;
