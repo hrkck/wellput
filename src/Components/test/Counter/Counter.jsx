665f84f2d6bdf4b0fe6db43b20b93f1398db24a5
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-07-10 00:48:56 +0200

    When button clicked, the box blinks.

--- src/Components/test/Counter/Counter.jsx
+++ src/Components/test/Counter/Counter.jsx
@@ -4,10 +4,13 @@ class Counter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            number: 0
+            number: 0,
+            blink: "alert alert-info"
         }
         this.increase.bind(this);
         this.decrease = this.decrease.bind(this);
+        this.blinkNormal.bind(this);
+        this.blinkRed.bind(this);
     }
 
     increase = () => {
@@ -17,15 +20,23 @@ class Counter extends Component {
     decrease(){
         this.setState({number: this.state.number - 1})
     }
+
+    blinkRed = () => {
+        this.setState({blink: "alert alert-danger"});
+    }
+
+    blinkNormal = () => {
+        this.setState({blink: "alert alert-info"});
+    }
     
     render() {
         return (
             <div>
-                <p className="alert alert-info" >
+                <p  className={this.state.blink} >
                     {this.state.number}
                 </p>
-                <input type="button" value="+" onClick={this.increase}/>
-                <input type="button" value="-" onClick={this.decrease}/>
+                <input type="button" value="+" onClick={this.increase} onMouseDown={this.blinkRed} onMouseUp={this.blinkNormal}/>
+                <input type="button" value="-" onClick={this.decrease} onMouseDown={this.blinkRed} onMouseUp={this.blinkNormal}/>
             </div>
         );
     }
