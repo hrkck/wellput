e34700cd58535040c06b2025f2e648a8fa31a235
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-08-13 19:36:54 +0200

    important comment added.

--- src/App.jsx
+++ src/App.jsx
@@ -40,3 +40,12 @@ class App extends Component {
 }
 
 export default App;
+
+
+/*
+    Consider above how I pass extra `props` to the `SubNav` component (stateless),
+    whilst sacrificing some built-in `props` such as `match`, provided by `React-Router`.
+    I did some lookup about that issue, found some answer but did not quite work for me.
+    It MIGHT be because I am using stateless component (`SubNav`).
+    Needs more experimenting.
+*/
