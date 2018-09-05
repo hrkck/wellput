da221e1bec9a6b886af07970afa60e1712522cf7
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-09-05 13:40:01 +0200

    minor fixes.

--- src/Components/core/Page/Page.jsx
+++ src/Components/core/Page/Page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = (props) => {
     return (
         <div className={`container shadow border-radius-4 px-4 px-md-5 py-4 py-md-3 ${props.className}`}>
-            <div className="row minHeight-75">
+            <div className="minHeight-75">
                 {props.children}
             </div>
         </div>
