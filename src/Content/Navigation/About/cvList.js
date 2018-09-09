bdde3ab24f3da6b33b40e4c60d408ffd4ad951ec
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-09-09 13:55:57 +0200

    tidy code

--- src/Content/Navigation/About/cvList.js
+++ src/Content/Navigation/About/cvList.js
@@ -6,13 +6,11 @@ export const cvList = [
         title: "English CV",
         url: "english-cv",
         component: englishCV
-    },
-    {
+    }, {
         title: "German CV",
         url: "german-cv",
         component: germanCV
-    },
-    {
+    }, {
         title: "Not Found",
         url: ":NotFound",
         component: NotFound
--- src/Content/Pages/About/CV/CV.jsx
+++ src/Content/Pages/About/CV/CV.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './assets/CV.css';
 
 export const englishCV = () => {
-    return (
-        <iframe className="embed-responsive" title="english-cv" src={require("./assets/cv-hakkirizakucuk-english.pdf")}/>
-    )
+    return (<iframe
+        className="embed-responsive"
+        title="english-cv"
+        src={require("./assets/cv-hakkirizakucuk-english.pdf")}/>)
 }
 
 export const germanCV = () => {
-    return (
-        <iframe className="embed-responsive" title="deutsch-cv" src={require("./assets/cv-hakkirizakucuk-deutsch.pdf")}/>
-    )
+    return (<iframe
+        className="embed-responsive"
+        title="deutsch-cv"
+        src={require("./assets/cv-hakkirizakucuk-deutsch.pdf")}/>)
 }
