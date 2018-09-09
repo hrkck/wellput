e949cd2accf6e946951bc4f90d2be8fe2fb1641c
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-09-09 13:56:28 +0200

    delete unneeded code

--- src/Content/Pages/Home/Home.jsx
+++ src/Content/Pages/Home/Home.jsx
@@ -4,13 +4,17 @@ import Page from '../../../Components/Page/Page';
 
 const Home = () => (
     <Page>
-        <div className="container-fluid">
-            <div className="row">
-                <h2>Home</h2>
-                <div className="col py-2">
-                    <iframe className="float-right border-radius-3" title="banging_dog" src="https://coub.com/embed/15oj4j?muted=false&autostart=false&originalSize=false&startWithHD=false" allowFullScreen frameBorder="0" width="320px" height="400px"></iframe><script async src="//c-cdn.coub.com/embed-runner.js"></script>
-                </div>
-            </div>
+        <h2>Home</h2>
+        <div className="col py-2 ml-3">
+            <iframe
+                className="float-right border-radius-3"
+                title="banging_dog"
+                src="https://coub.com/embed/15oj4j?muted=false&autostart=false&originalSize=false&startWithHD=false"
+                allowFullScreen
+                frameBorder="0"
+                width="320px"
+                height="400px"></iframe>
+            <script async src="//c-cdn.coub.com/embed-runner.js"></script>
         </div>
     </Page>
 );
