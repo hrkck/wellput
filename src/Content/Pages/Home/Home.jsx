6e1c1cd49dce2177b5e5c5e666c14484a2648960
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-09-24 13:26:19 +0200

    deploying automatically to master branch.

--- src/Content/Pages/Home/Home.jsx
+++ src/Content/Pages/Home/Home.jsx
@@ -5,13 +5,15 @@ import Page from '../../../Components/Page/Page';
 const Home = () => (
     <Page>
         <h2>Home</h2>
-        <p>Final test and accepted work-flow:
-            <ol>
-                <li>Make changes in <pre>source</pre> branch</li>
-                <li><pre>npm run deploy</pre> to build the site on <pre>gh-pages</pre> branch.</li>
-                <li>Merge <pre>gh-pages</pre> onto <pre>master</pre>. Push <pre>master</pre> branch.</li>
-            </ol>
-        </p>
+        <strike>
+            <p>Final test and accepted work-flow:
+                <ol>
+                    <li>Make changes in <code>source</code> branch</li>
+                    <li><code>npm run deploy</code> to build the site on <code>gh-pages</code> branch.</li>
+                    <li>Merge <code>gh-pages</code> onto <code>master</code>. Push <code>master</code> branch.</li>
+                </ol>
+            </p>
+        </strike>
         <div className="col py-2 ml-3">
             <iframe
                 className="float-right border-radius-3"
