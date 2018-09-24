858ee5037efe6142a48a37b5aba0031c828c3c36
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-09-24 12:15:29 +0200

    add content about page.

--- src/Content/Pages/About/About.jsx
+++ src/Content/Pages/About/About.jsx
@@ -9,6 +9,7 @@ const About = () => {
             <h3>About me...</h3>
             <p>For an insight, check my cv.</p>
             <a href={require("./CV/assets/cv-hakkirizakucuk-english.pdf")} target="_blank">CV</a>
+            <p>I work on this website to put the weight off of mind.</p>
         </Page>
     )
 };
