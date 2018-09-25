3c6214d01a3ca2f35450bcd732690eee6477df07
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-09-25 13:14:32 +0200

    code clean

--- src/Content/Pages/Projects/FCCProjects/Projects/Projects.jsx
+++ src/Content/Pages/Projects/FCCProjects/Projects/Projects.jsx
@@ -1,131 +1,44 @@
 import React from 'react';
 
+const CodePenEmbed = ({title, src}) => 
+    <div>
+        <iframe 
+            className="minHeight-75 w-100"
+            title={title}
+            src={`${src}result&embed-version=2`}
+            frameborder="no"
+            allowtransparency='true'
+            allowFullScreen={true}
+        />
+        See the pen <a href={`https:${src}`} target="_blank" rel="noopener noreferrer"> FCC_Portfolio </a> by <a href='https://codepen.io/hrkucuk' target="_blank" rel="noopener noreferrer">@hrkucuk</a> on <a href='https://codepen.io' target="_blank" rel="noopener noreferrer"> CodePen</a>.
+    </div>
+
 export const Portfolio = () => {
     return (
-        <div>
-            <iframe
-                className="minHeight-75 w-100 border-radius-4"
-                frameBorder="no"
-                scrolling='no'
-                title='FCC_Portfolio'
-                src='//codepen.io/hrkucuk/embed/LBjpZx/result&embed-version=2'
-                allowtransparency='true'
-                allowFullScreen={true}
-                style={{
-                width: '100%'
-            }}></iframe>
-            See the Pen
-            <a
-                href='https://codepen.io/hrkucuk/pen/LBjpZx/'
-                target="_blank"
-                rel="noopener noreferrer">FCC_Portfolio</a>
-            by hrkucuk (<a href='https://codepen.io/hrkucuk' target="_blank" rel="noopener noreferrer">@hrkucuk</a>) on
-            <a href='https://codepen.io' target="_blank" rel="noopener noreferrer">CodePen</a>.
-
-        </div>
+        <CodePenEmbed title="FCC_Portfolio" src="//codepen.io/hrkucuk/embed/LBjpZx/"/>
     );
 };
 
 export const TechnicalDocumentationPage = () => {
     return (
-        <div>
-            <iframe
-                className="minHeight-75 w-100 border-radius-4"
-                frameBorder="no"
-                scrolling='no'
-                title='FCC_TechnicalDocumentationPage'
-                src='//codepen.io/hrkucuk/embed/RBVzay/result&embed-version=2'
-                allowtransparency='true'
-                allowFullScreen={true}
-                style={{
-                width: '100%'
-            }}></iframe>
-            See the Pen
-            <a
-                href='https://codepen.io/hrkucuk/pen/RBVzay/'
-                target="_blank"
-                rel="noopener noreferrer">FCC_TechnicalDocumentationPage</a>
-            by hrkucuk (<a href='https://codepen.io/hrkucuk' target="_blank" rel="noopener noreferrer">@hrkucuk</a>) on
-            <a href='https://codepen.io' target="_blank" rel="noopener noreferrer">CodePen</a>.
-
-        </div>
+        <CodePenEmbed title="FCC_TechnicalDocumentationPage" src="//codepen.io/hrkucuk/embed/RBVzay/"/>
     );
 };
 
 export const ProductLandingPage = () => {
     return (
-        <div>
-            <iframe
-                className="minHeight-75 w-100 border-radius-4"
-                frameBorder="no"
-                scrolling='no'
-                title='FCC_ProductLandingPage'
-                src='//codepen.io/hrkucuk/embed/ZjKPrB/result&embed-version=2'
-                allowtransparency='true'
-                allowFullScreen={true}
-                style={{
-                width: '100%'
-            }}></iframe>
-            See the Pen
-            <a
-                href='https://codepen.io/hrkucuk/pen/ZjKPrB/'
-                target="_blank"
-                rel="noopener noreferrer">FCC_ProductLandingPage</a>
-            by hrkucuk (<a href='https://codepen.io/hrkucuk' target="_blank" rel="noopener noreferrer">@hrkucuk</a>) on
-            <a href='https://codepen.io' target="_blank" rel="noopener noreferrer">CodePen</a>.
-
-        </div>
+        <CodePenEmbed title="FCC_ProductLandingPage" src="//codepen.io/hrkucuk/embed/ZjKPrB/"/>
     );
 };
 
 export const SurveyForm = () => {
     return (
-        <div>
-            <iframe
-                className="minHeight-75 w-100 border-radius-4"
-                frameBorder="no"
-                scrolling='no'
-                title='FCC_SurveyForm'
-                src='//codepen.io/hrkucuk/embed/qymNXo/result&embed-version=2'
-                allowtransparency='true'
-                allowFullScreen={true}
-                style={{
-                width: '100%'
-            }}></iframe>
-            See the Pen
-            <a
-                href='https://codepen.io/hrkucuk/pen/qymNXo/'
-                target="_blank"
-                rel="noopener noreferrer">FCC_SurveyForm</a>
-            by hrkucuk (<a href='https://codepen.io/hrkucuk' target="_blank" rel="noopener noreferrer">@hrkucuk</a>) on
-            <a href='https://codepen.io' target="_blank" rel="noopener noreferrer">CodePen</a>.
-
-        </div>
+        <CodePenEmbed title="FCC_SurveyForm" src="//codepen.io/hrkucuk/embed/qymNXo/"/>
     );
 };
 
 export const TributePage = () => {
     return (
-        <div>
-            <iframe
-                className="minHeight-75 w-100 border-radius-4"
-                frameBorder="no"
-                scrolling='no'
-                title='FCC_TributePage'
-                src='//codepen.io/hrkucuk/embed/QBvwqq/result&embed-version=2'
-                allowtransparency='true'
-                allowFullScreen={true}
-                style={{
-                width: '100%'
-            }}></iframe>
-            See the Pen
-            <a
-                href='https://codepen.io/hrkucuk/pen/QBvwqq/'
-                target="_blank"
-                rel="noopener noreferrer">FCC_TributePage</a>
-            by hrkucuk (<a href='https://codepen.io/hrkucuk' target="_blank" rel="noopener noreferrer">@hrkucuk</a>) on
-            <a href='https://codepen.io' target="_blank" rel="noopener noreferrer">CodePen</a>.
-
-        </div>
+        <CodePenEmbed title="FCC_TributePage" src="//codepen.io/hrkucuk/embed/QBvwqq/"/>
     );
 };
