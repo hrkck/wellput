7f697d4f145ac662df05a54662c17f5801239446
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-09-25 12:48:58 +0200

    citation added.

--- src/Content/Pages/Home/Home.jsx
+++ src/Content/Pages/Home/Home.jsx
@@ -4,12 +4,19 @@ import Page from '../../../Components/Page/Page';
 
 const Home = () => (
     <Page>
-        
-        <code className="d-block text-center">
-            Good programmers write good code. <br/>
-            Great programmers write no code. <br/>
-            Zen programmers delete code.
-        </code>
+
+        <blockquote className="d-block text-center">
+            Good programmers write good code. <br />
+            Great programmers write no code. <br />
+            Zen programmers delete code. <br />
+            <cite>
+                <a target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.quora.com/What-are-some-things-that-only-someone-who-has-been-programming-20-50-years-would-know">
+                    - John Byrd
+                </a>
+            </cite>
+        </blockquote>
 
     </Page>
 );
