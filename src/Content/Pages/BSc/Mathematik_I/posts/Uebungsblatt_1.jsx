ed8652703c4e031911d766c2b8c26f9d1fdc4092
Hakki Riza Kucuk <hakkirizakucuk@yandex.com>
2018-09-29 15:27:45 +0200

    add outline for exercise 1

--- src/Content/Pages/BSc/Mathematik_I/posts/Uebungsblatt_1.jsx
+++ src/Content/Pages/BSc/Mathematik_I/posts/Uebungsblatt_1.jsx
@@ -5,7 +5,43 @@ import MathJax from 'react-mathjax2'
 const Uebungsblatt_1 = () => {
     return (
         <div>
-            
+            <h1>Übungsblatt 1</h1>
+
+            <p>In den ersten Übungsblatt gibt es 11 Aufgaben über verschiedene Begriffen. So geht es:</p>
+
+            <h4>Aufgabe 1</h4>
+            <p>Brüche, Potenzen, Wurzeln, Logarithmen und Absolutbeträge <br/> (Kesirler, üslü sayılar, kökler, logaritmalar ve mutlak değerler)</p>
+
+            <h4>Aufgabe 2</h4>
+            <p>Gleichung einer Gerade <br/> (lineer denklem)</p>
+
+            <h4>Aufgabe 3</h4>
+            <p>Polynomdivion <br/> (Polinom bölümü)</p>
+
+            <h4>Aufgabe 4</h4>
+            <p>Ausklammern der grössten Potenz <br/> (en büyük çarpanlarına ayırma)</p>
+
+            <h4>Aufgabe 5</h4>
+            <p>Lösen von Gleichungen, einige Grundprinzipien <br/> (Denklem çözme, bazı kurallar) </p>
+
+            <h4>Aufgabe 6</h4>
+            <p>Gleichungen <br/> (Denklemler)</p>
+
+            <h4>Aufgabe 7</h4>
+            <p>Quadratische Gleichung mit Parameter (ikinci dereceden parametrik denklem)</p>
+
+            <h4>Aufgabe 8</h4>
+            <p>Fakultäten und Binomialkoeffizenten <br/> (Faktöriyel ve binom açılımı)</p>
+
+            <h4>Aufhabe 9</h4>
+            <p>Summen und Produkte <br/> (Toplama ve Çarpma sembolleri)</p>
+
+            <h4>Aufhabe 10</h4>
+            <p>Summenzeichen, anwendung <br/> (Toplama işareti, uygulama)</p>
+
+            <h4>Aufgabe 11</h4>
+            <p>grobe Schätzung des Logarithmus <br/> (Logaritmanın yaklaşık tahmini)</p>
+
         </div>
     );
 };
