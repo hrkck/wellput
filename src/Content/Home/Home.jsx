b88704fe37eb1172c6b83d8890b4ce6aa3ae06be
hakki riza kucuk <hakkirizakucuk@yandex.com>
2019-01-28 13:02:18 +0100

    UPDATE: home page content

--- src/Content/Home/Home.jsx
+++ src/Content/Home/Home.jsx
@@ -11,6 +11,10 @@ const Home = () => (
             <a target="_blank" rel="noopener noreferrer" href="https://www.quora.com/What-are-some-things-that-only-someone-who-has-been-programming-20-50-years-would-know">
                 <em>- John Byrd</em>
             </a>
+
+            <br/><br/><br/><br/><br/>
+
+            <p>This project is on suspense. Now I am <a href="http://archetypum.github.io" target="_blank" rel="noopener noreferrer">learning</a> <a href="http://mithril.js.org" target="_blank" rel="noopener noreferrer">Mithril js</a> .</p>
         </div>
     </Page>
 );
